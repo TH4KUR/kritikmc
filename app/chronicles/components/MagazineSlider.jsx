a7e54cb004288854ffd743e62037195c957f78be
TH4KUR <max910payne@gmail.com>
2024-06-14 20:36:04 +0530

    Added functional carousel

--- app/chronicles/components/MagazineSlider.jsx
+++ app/chronicles/components/MagazineSlider.jsx
@@ -1,25 +1,78 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React from "react";
+import { useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 
 function EmblaCarousel() {
-  const [emblaRef] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
 
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
   return (
-    <div className="embla mt-9" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide rounded-xl overflow-hidden">
-          <img src="/v1.webp" alt="magazine cover" />
+    <>
+      <div className="flex items-center gap-3">
+        <button className="embla__prev relative z-[100]" onClick={scrollPrev}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="#000000"
+            viewBox="0 0 256 256"
+          >
+            <path d="M128,20A108,108,0,1,0,236,128,108.12,108.12,0,0,0,128,20Zm0,192a84,84,0,1,1,84-84A84.09,84.09,0,0,1,128,212ZM152.49,96.49,121,128l31.52,31.51a12,12,0,0,1-17,17l-40-40a12,12,0,0,1,0-17l40-40a12,12,0,0,1,17,17Z"></path>
+          </svg>
+        </button>
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container">
+            <button
+              className="embla__slide"
+              onClick={() => {
+                console.log("Clicked");
+              }}
+            >
+              <img src="/v1.webp" alt="magazine cover" />
+              <span className="mt-3 font-semibold">Issue 1</span>
+              <span className="text-xs">July 2023</span>
+            </button>
+            <button
+              className="embla__slide"
+              onClick={() => {
+                console.log("Clicked");
+              }}
+            >
+              <img src="/v2.webp" alt="magazine cover" />
+              <span className="mt-3 font-semibold">Issue 2</span>
+              <span className="text-xs">November 2023</span>
+            </button>
+            <button
+              className="embla__slide"
+              onClick={() => {
+                console.log("Clicked");
+              }}
+            >
+              <img src="/v3.webp" alt="magazine cover" />
+              <span className="mt-3 font-semibold">Issue 3</span>
+              <span className="text-xs">May 2024</span>
+            </button>
+          </div>
         </div>
-        <div className="embla__slide rounded-xl overflow-hidden ">
-          <img src="/v2.webp" alt="magazine cover" />
-        </div>
-        <div className="embla__slide rounded-xl overflow-hidden ">
-          <img src="/v3.webp" alt="magazine cover" />
-        </div>
+        <button className="embla__next relative z-[100]" onClick={scrollNext}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="#000000"
+            viewBox="0 0 256 256"
+          >
+            <path d="M128,20A108,108,0,1,0,236,128,108.12,108.12,0,0,0,128,20Zm0,192a84,84,0,1,1,84-84A84.09,84.09,0,0,1,128,212Zm32.49-92.49a12,12,0,0,1,0,17l-40,40a12,12,0,0,1-17-17L135,128,103.51,96.49a12,12,0,0,1,17-17Z"></path>
+          </svg>
+        </button>
       </div>
-    </div>
+    </>
   );
 }
 
