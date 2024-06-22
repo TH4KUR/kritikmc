33825a8d6225fbbc454df76e2e99915d7cd2db5d
TH4KUR <max910payne@gmail.com>
2024-06-22 08:28:55 +0530

    more design changes

--- app/events/components/VerticalTab.jsx
+++ app/events/components/VerticalTab.jsx
@@ -1,15 +1,52 @@
-import React, { useState } from "react";
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 import VTlist from "./VTlist";
-import VTcontent from "./VTcontent";
 import "./VTStyles.css";
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, scale: 0 },
+};
+
+// const [ref, inView] = useInView();
+
+// useEffect(() => {
+//   if (inView) {
+//     control.start("visible");
+//   } else {
+//     control.start("hidden");
+//   }
+// }, [control, inView]);
+
+const Box = ({ wrapper, tabId, setTab }) => {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0.5,
+    root: wrapper.current,
+  });
+  useEffect(() => {
+    if (inView === true) {
+      setTab(tabId);
+    }
+  }, [inView]);
+  return (
+    <div
+      ref={ref}
+      className="snap-center bg-red-500 h-[50vh] grid place-items-center"
+    >
+      VerticalTab {inView ? "yup" : "nope"}
+    </div>
+  );
+};
 
 function VerticalTab(props) {
   const [activeTabId, setActiveTabId] = useState(0);
-
   function btnClick(id) {
     setActiveTabId(id);
   }
-
+  const wrapperRef = useRef(null);
   return (
     <div className="section__Jobs-container">
       <div className="flex gap-2">
@@ -28,15 +65,16 @@ function VerticalTab(props) {
             </ul>
           </div>
         </div>
-        <div>
-          {props.data.map((job, index) => (
-            <VTcontent
-              data={job}
-              key={index}
-              index={index}
-              activeTabId={activeTabId}
-            />
-          ))}
+        <div
+          ref={wrapperRef}
+          className="h-[50vh] snap-y snap-mandatory overflow-y-scroll w-full"
+        >
+          <Box setTab={setActiveTabId} tabId={0} wrapper={wrapperRef} />
+          <Box setTab={setActiveTabId} tabId={1} wrapper={wrapperRef} />
+          <Box setTab={setActiveTabId} tabId={2} wrapper={wrapperRef} />
+          <Box setTab={setActiveTabId} tabId={3} wrapper={wrapperRef} />
+          <Box setTab={setActiveTabId} tabId={4} wrapper={wrapperRef} />
+          <Box setTab={setActiveTabId} tabId={5} wrapper={wrapperRef} />
         </div>
       </div>
       <span
