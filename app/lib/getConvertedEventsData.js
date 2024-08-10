666cd38b1263f268919a8b023d4516811852c004
TH4KUR <max910payne@gmail.com>
2024-08-10 19:14:55 +0530

    date time patch 2

--- app/lib/getConvertedEventsData.js
+++ app/lib/getConvertedEventsData.js
@@ -26,10 +26,14 @@ export default async function getConvertedEventsData() {
         // let endHours = `${new Date(el.endTime).getHours() < 10 ? `0${new Date(el.endTime).getHours()}` : new Date(el.endTime).getHours()}`;
         // let endMinutes = `${new Date(el.endTime).getMinutes() < 10 ? `0${new Date(el.endTime).getMinutes()}` : new Date(el.endTime).getMinutes()}`;
         const startTimeArr = new Date(el.startTime)
-          .toLocaleTimeString("en-IN")
+          .toLocaleTimeString({
+            timezone: "Asia/Kolkata",
+          })
           .split(":");
         const endTimeArr = new Date(el.endTime)
-          .toLocaleTimeString("en-IN")
+          .toLocaleTimeString({
+            timezone: "Asia/Kolkata",
+          })
           .split(":");
         return {
           time: `${startTimeArr[0] + ":" + startTimeArr[1] + startTimeArr[2].slice(-2)}- ${endTimeArr[0] + ":" + endTimeArr[1] + endTimeArr[2].slice(-2)}`,
