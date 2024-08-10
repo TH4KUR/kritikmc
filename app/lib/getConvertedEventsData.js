9bb0f80f40008fe538d16ad1de379f46e1b94776
TH4KUR <max910payne@gmail.com>
2024-08-10 19:23:12 +0530

    not proud of how i solved it :( but hey it works!

--- app/lib/getConvertedEventsData.js
+++ app/lib/getConvertedEventsData.js
@@ -5,12 +5,12 @@ export default async function getConvertedEventsData() {
     query: `*[_type == 'conferenceBreakdown']{
     conferenceDay,conferenceDate,
     timeslots[]{
-      startTime,endTime,postNriLunch,
+      cardTitle,startTime,endTime,postNriLunch,
       events[]->{
         eventName,
       },
     },
-  }`,
+    }`,
   });
 
   const parsedRes = res.map((obj, i) => {
@@ -20,23 +20,8 @@ export default async function getConvertedEventsData() {
       day: obj.conferenceDay,
       date: obj.conferenceDate,
       schedule: obj.timeslots.map((el, j) => {
-        // let startHours = `${new Date(el.startTime).getHours() < 10 ? `0${new Date(el.startTime).getHours()}` : new Date(el.startTime).getHours()}`;
-        // let startMinutes = `${new Date(el.startTime).getMinutes() < 10 ? `0${new Date(el.startTime).getMinutes()}` : new Date(el.startTime).getMinutes()}`;
-
-        // let endHours = `${new Date(el.endTime).getHours() < 10 ? `0${new Date(el.endTime).getHours()}` : new Date(el.endTime).getHours()}`;
-        // let endMinutes = `${new Date(el.endTime).getMinutes() < 10 ? `0${new Date(el.endTime).getMinutes()}` : new Date(el.endTime).getMinutes()}`;
-        const startTimeArr = new Date(el.startTime)
-          .toLocaleTimeString({
-            timezone: "Asia/Kolkata",
-          })
-          .split(":");
-        const endTimeArr = new Date(el.endTime)
-          .toLocaleTimeString({
-            timezone: "Asia/Kolkata",
-          })
-          .split(":");
         return {
-          time: `${startTimeArr[0] + ":" + startTimeArr[1] + startTimeArr[2].slice(-2)}- ${endTimeArr[0] + ":" + endTimeArr[1] + endTimeArr[2].slice(-2)}`,
+          time: `${el.cardTitle}`,
           postNriLunch: el.postNriLunch === "Yes" ? true : false,
           events: el.events.map(({ eventName }) => eventName),
         };
