02443a1183505c0cd0ab809da8a3641e7e19d627
TH4KUR <max910payne@gmail.com>
2024-09-04 19:28:57 +0530

    bug fixed!!

--- app/lib/UploadRegistrationData.js
+++ app/lib/UploadRegistrationData.js
@@ -5,16 +5,16 @@ export async function UploadRegistrationData(regData, imgurl) {
   await connectDB();
   console.log(imgurl);
   const studentData = new student({
-    delegateId: regData.delegateId,
-    name: regData.studentName,
-    email: regData.studentEmail,
-    mobileno: Number(regData.studentNumber),
-    collegeYear: regData.collegeYear ? Number(regData.collegeYear) : 0,
-    collegeName: regData.studentCollege,
-    isKmcStudent: regData.isKmcStudent === "true" ? true : false,
-    isPgStudent: regData.isPgStudent === "true" ? true : false,
-    kmcRollNo: regData.kmcRollNo,
-    events: regData.events,
+    delegateId: regData?.delegateId,
+    name: regData?.studentName,
+    email: regData?.studentEmail,
+    mobileno: Number(regData?.studentNumber),
+    collegeYear: regData?.collegeYear ? Number(regData?.collegeYear) : 0,
+    collegeName: regData?.studentCollege,
+    isKmcStudent: regData?.isKmcStudent === "true" ? true : false,
+    isPgStudent: regData?.isPgStudent === "true" ? true : false,
+    kmcRollNo: regData?.kmcRollNo,
+    events: regData?.events,
     screenshotLink: imgurl,
   });
   console.log("Uploading data:", studentData);
