626ad4300b4840e7147aa7d18ddaa99274c13a44
TH4KUR <max910payne@gmail.com>
2025-11-10 22:43:02 +0530

    build fix

--- app/lib/supabase/supabaseAdmin.js
+++ app/lib/supabase/supabaseAdmin.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL;
-
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
