c190e41c993def55eb3859e0ad0446d48dc9b24a
Eashaan Thakur <81467048+TH4KUR@users.noreply.github.com>
2025-12-09 21:45:34 +0530

    Reduce ACTIVE_PG_FEE from 1000 to 750

--- app/lib/paymentConfig.js
+++ app/lib/paymentConfig.js
@@ -1,6 +1,6 @@
 export const ACTIVE_NON_KMC_FEE = 600;
 export const ACTIVE_KMC_FEE = 500;
-export const ACTIVE_PG_FEE = 1000;
+export const ACTIVE_PG_FEE = 750;
 export const PASSIVE_DELEGATE_FEE = 300;
 export const WORKSHOP_FEE = 550;
 
